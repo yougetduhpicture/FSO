0b2470bc6cf80f28f93b9a1a750b194e1d3ca633
Rasmus Ruonakoski <ruonakoski.rasmus@gmail.com>
2026-02-09 11:27:16 +0200

    9.3 bmi calculator (1/2 done)
    Now it works from the command line. I used the same parsing as the multiplier. Also made the code a bit more readable by storing the bmi on it's own value.

--- 9/bmiCalculator.ts
+++ 9/bmiCalculator.ts
@@ -1,13 +1,45 @@
+interface Values {
+  height: number;
+  weight: number;
+}
+
+const parseArgs = (args: string[]): Values => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
+
 const calculateBmi = (height: number, weight: number) => {
-    if ((weight / (height / 100)**2) < 18.5) {
-        return 'low range'
-     } else if ((weight / (height / 100)**2) < 25) {
-        return 'normal range'
-     } else if ((weight / (height / 100)**2) < 30) {
-        return 'high range'
+   let bmi = (weight / (height / 100)**2);
+
+    if (bmi < 18.5) {
+        console.log('low range');
+     } else if (bmi < 25) {
+        console.log('normal range');
+     } else if (25 < bmi && bmi < 30) {
+        console.log('high range');
      } else {
-        return 'highest range'
+        console.log('highest range');
      }
 }
 
-console.log(calculateBmi(180, 100))
+
+try {
+  const { height, weight } = parseArgs(process.argv);
+  calculateBmi(height, weight);
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
